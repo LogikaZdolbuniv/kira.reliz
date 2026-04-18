8cce4a3b7878f9ffc79e40eb1dc5138403b88a6d
LogikaZdolbuniv <vv1ktoriababenko@gmail.com>
2026-04-18 11:23:19 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -4,7 +4,12 @@ const flowers = [
     { id: 3, name: "Весняні Тюльпани", price: 600, img: "tulpany.jpg"},
     { id: 4, name: "Білі Лілії", price: 950, img: "lily.jpg" },
     { id: 5, name: "Букет Лаванди", price: 450, img: "https://images.unsplash.com/photo-1565011523534-747a8601f10a?q=80&w=600" },
-    { id: 6, name: "Соняшники", price: 700, img: "https://images.unsplash.com/photo-1470509037663-253afd7f0f51?q=80&w=600" }
+    { id: 6, name: "Соняшники", price: 700, img: "https://images.unsplash.com/photo-1470509037663-253afd7f0f51?q=80&w=600" },
+    { id: 7, name: "Еустоми", price: 868, img: "eustome.png"  },
+    { id: 8, name: "Ранункулюси", price: 706, img: "images(6).jpg" },
+    { id: 9, name: "Гіпсофіли", price: 1000, img: "gipsofil.jpg" },
+    { id: 10, name: "Гвоздики", price: 530, img: "gvozdics.jpg" },
+    { id: 11, name: "Бузок", price: 746, img: "buzoc.jpg" }
 ];
 
 let cart = [];
